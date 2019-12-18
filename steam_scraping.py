f349cf3c8fb9f5c851db9074ed9e5754ca03d06f
Vangarooo Yamada <vangarooo.yamada@gmail.com>
2019-12-18 20:51:12 +0900

    Fix SteamUser class
    fix id|custom extracting

--- steam_scraping.py
+++ steam_scraping.py
@@ -12,21 +12,25 @@ pm = urllib3.PoolManager(
 
 class SteamUser:
     def __init__(self, url):
-        self.name = re.match(r'(id|profile)\/(.+)\/').groups(2)
-        self.res = pm.request('GET', url)
+        self.name = re.search(r'(id|profile)\/(.+)\/?', url).groups(2)
+        self.res = pm.request('GET', url + '/friends/')
         self.soup = BeautifulSoup(self.res.data, 'html.parser')
         self.fr_list = list(self.soup.find_all('a', class_='selectable_overlay'))
 
     def show_friends(self):
         for n in self.fr_list:
-            print(n)
+            print(n.attrs['href'])
 
     def check_friend(self):
         pass
 
 
 if __name__ == '__main__':
-    url = 'https://steamcommunity.com/id/vangarooo/friends/'
+    # Test ****************
+    url = input('Input steamURL: ')
+
+    p1 = SteamUser(url)
+    p1.show_friends()
 
     # for f in friends_list:
     #     print(f.attrs['href'])
