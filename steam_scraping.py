c3112ef092f409480cd78a7e31f9c57d9862e434
Vangarooo Yamada <vangarooo.yamada@gmail.com>
2019-12-18 19:15:47 +0900

    Add SteamUser class

--- steam_scraping.py
+++ steam_scraping.py
@@ -1,19 +1,32 @@
+import re
 import urllib3
 import certifi
 from bs4 import BeautifulSoup
 
 
-url = 'https://steamcommunity.com/id/vangarooo/friends/'
-
 pm = urllib3.PoolManager(
     cert_reqs='CERT_REQUIRED',
     ca_certs=certifi.where()
 )
-res = pm.request('GET', url)
-# print(res.data)
-soup = BeautifulSoup(res.data, 'html.parser')
 
-friends_list = list(soup.find_all('a', class_='selectable_overlay'))
 
-for f in friends_list:
-    print(f.attrs['href'])
+class SteamUser:
+    def __init__(self, url):
+        self.name = re.match(r'(id|profile)\/(.+)\/').groups(2)
+        self.res = pm.request('GET', url)
+        self.soup = BeautifulSoup(self.res.data, 'html.parser')
+        self.fr_list = list(self.soup.find_all('a', class_='selectable_overlay'))
+
+    def show_friends(self):
+        for n in self.fr_list:
+            print(n)
+
+    def check_friend(self):
+        pass
+
+
+if __name__ == '__main__':
+    url = 'https://steamcommunity.com/id/vangarooo/friends/'
+
+    # for f in friends_list:
+    #     print(f.attrs['href'])
