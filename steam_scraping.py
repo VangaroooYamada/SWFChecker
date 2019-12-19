4ff074e118be14e00e832ebb44ad14d18e24ea06
Vangarooo Yamada <vangarooo.yamada@gmail.com>
2019-12-19 15:44:10 +0900

    Improve Some methods
    improve SteamUser class' __init__ and show_friends methods

--- steam_scraping.py
+++ steam_scraping.py
@@ -8,19 +8,22 @@ pm = urllib3.PoolManager(
     cert_reqs='CERT_REQUIRED',
     ca_certs=certifi.where()
 )
-id_cmp = re.compile(r'(id|profile)\/(.+)\/?')
+id_cmp = re.compile(r'(id|profiles)\/(.+)\/?')
 
 
 class SteamUser:
     def __init__(self, url):
-        self.name = id_cmp.search(url).groups(2)
+        self.name = id_cmp.search(url).groups()[1]
         self.res = pm.request('GET', url + '/friends/')
         self.soup = BeautifulSoup(self.res.data, 'html.parser')
-        self.fr_list = list(self.soup.find_all('a', class_='selectable_overlay'))
+        self.fr_list = [
+            id_cmp.search(fr.attrs['href']).groups()[1] for fr
+            in self.soup.find_all('a', class_='selectable_overlay')]
 
     def show_friends(self):
+        print(f'My name is {self.name}')
         for n in self.fr_list:
-            print(n.attrs['href'])
+            print(n)
 
     def check_friend(self):
         pass
@@ -32,6 +35,3 @@ if __name__ == '__main__':
 
     p1 = SteamUser(url)
     p1.show_friends()
-
-    # for f in friends_list:
-    #     print(f.attrs['href'])
